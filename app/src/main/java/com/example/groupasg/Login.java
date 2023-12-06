a928c033e9fe86fa4fc6450a8bd8b43db5d8f0eb
LEONG-02 <chenlong200219@gmail.com>
2023-12-06 14:12:44 +0800

    Animation Splash Screen, Login, Register, Forget Password

--- app/src/main/java/com/example/groupasg/Login.java
+++ app/src/main/java/com/example/groupasg/Login.java
@@ -1,20 +1,18 @@
 package com.example.groupasg;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.SpannableString;
 import android.text.TextUtils;
 import android.text.method.PasswordTransformationMethod;
-import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,10 +21,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import org.w3c.dom.Text;
-
 public class Login extends AppCompatActivity {
-
     TextInputEditText et_email, et_password;
     Button buttonSignUp, buttonLogin;
     FirebaseAuth mAuth;
@@ -38,13 +33,12 @@ public class Login extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
             finish();
         }
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +58,7 @@ public class Login extends AppCompatActivity {
         tvForget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(getApplicationContext(),forgetpassword.class);
+                Intent intent = new Intent(getApplicationContext(), forgetpassword.class);
                 startActivity(intent);
                 finish();
             }
@@ -73,7 +67,7 @@ public class Login extends AppCompatActivity {
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(getApplicationContext(),Register.class);
+                Intent intent = new Intent(getApplicationContext(), Register.class);
                 startActivity(intent);
                 finish();
             }
@@ -123,9 +117,9 @@ public class Login extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 progressBar.setVisibility(View.GONE);
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(getApplicationContext(),"Login Successfull",
+                                    Toast.makeText(getApplicationContext(), "Login Successfull",
                                             Toast.LENGTH_SHORT).show();
-                                    Intent intent= new Intent(getApplicationContext(),
+                                    Intent intent = new Intent(getApplicationContext(),
                                             MainActivity.class);
                                     startActivity(intent);
                                     finish();
